05cbf51e785a5a817a350847c1f40addad2d3a83
Arifalhakimi <21104039@ittelkom-pwt.ac.id>
2023-10-10 21:44:28 +0700

    cape

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
+// halaman login
+// tombol login di klik
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.querySelector(".btn-login button");
+
+    submitButton.addEventListener("click", function () {
+        // Ganti teks tombol dengan pesan "Loading..." saat tombol diklik
+        submitButton.innerHTML = "Loading...";
+        submitButton.disabled = true;
+
+        // Simulasi pengecekan login (Anda dapat menggantinya dengan logika sesungguhnya)
+        setTimeout(function () {
+            // Setelah beberapa waktu, kembalikan teks tombol ke semula
+            submitButton.innerHTML = "Submit";
+            submitButton.disabled = false;
+
+            // Redirect ke halaman lain atau lakukan tindakan login sesungguhnya
+            // window.location.href = "halaman-berikutnya.html";
+        }, 2000); // Contoh, tunggu 2 detik sebelum kembali ke teks awal
+    });
+});
+
+// tombol mata untuk melihat password
+const passwordField = document.getElementById('pwd');
+const togglePassword = document.getElementById('togglePassword');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    togglePassword.classList.toggle('fa-eye');
+    togglePassword.classList.toggle('fa-eye-slash');
+});
+
 // klik button diskusi
+
+
 // Fungsi untuk menampilkan form di tengah-tengah halaman
+
 function tampilkanForm() {
     var overlay = document.getElementById("overlay");
     var popupForm = document.getElementById("popup-form");
@@ -42,35 +78,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// halaman login
-// tombol login di klik
-document.addEventListener("DOMContentLoaded", function () {
-    const submitButton = document.querySelector(".btn-login button");
 
-    submitButton.addEventListener("click", function () {
-        // Ganti teks tombol dengan pesan "Loading..." saat tombol diklik
-        submitButton.innerHTML = "Loading...";
-        submitButton.disabled = true;
-
-        // Simulasi pengecekan login (Anda dapat menggantinya dengan logika sesungguhnya)
-        setTimeout(function () {
-            // Setelah beberapa waktu, kembalikan teks tombol ke semula
-            submitButton.innerHTML = "Submit";
-            submitButton.disabled = false;
-
-            // Redirect ke halaman lain atau lakukan tindakan login sesungguhnya
-            // window.location.href = "halaman-berikutnya.html";
-        }, 2000); // Contoh, tunggu 2 detik sebelum kembali ke teks awal
-    });
-});
-
-// tombol mata untuk melihat password
-const passwordField = document.getElementById('pwd');
-const togglePassword = document.getElementById('togglePassword');
-
-togglePassword.addEventListener('click', function () {
-    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordField.setAttribute('type', type);
-    togglePassword.classList.toggle('fa-eye');
-    togglePassword.classList.toggle('fa-eye-slash');
-});
